80134f487633c0780ca9dfcd8eea62734345207d
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:54:57 -0300

    refactor: adiciona barrel de exportação compartilhada
    ## Descrição
    
    - Adiciona arquivo de exportação para simplificar imports do frontend.
    - Impacto: melhora organização e reutilização dos módulos compartilhados.

---
+++ HomeBrokerSPA/HomeBrokerChart/src/app/shared/components/index.ts
@@ -0,0 +1,7 @@
+export * from './charts/line.chart/line.chart.component';
+export * from './charts/line.chart/line.chart.module';
+export * from './charts/macd-chart/macd.chart.component';
+export * from './charts/macd-chart/macd.chart.module';
+export * from './period-filter/period.filter.component';
+export * from './loading-component/loading.component';
+export * from './charts/chart.options/chart.common.options';
