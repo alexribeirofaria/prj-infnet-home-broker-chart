37abf3abbe46f254b1613925379550364f049fcd
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:55:04 -0300

    test: adiciona testes do MACD
    ## Descrição
    
    - Adiciona testes para o objeto de valor MACD.
    - Impacto: valida o indicador técnico usado na análise de preços.

---
+++ HomeBrokerXUnit/Domain/MACDTest.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Charts.ValueObject;
+using HomeBrokerXUnit.__mock__;
+
+namespace Domain;
+public sealed class MACDTest
+{
+    const int MIN_AMOUNT_DATA = 34;
+
+    [Fact]
+    public void Should_Create_MACD_Correctly()
+    {
+        // Arrange
+        var faker = MagazineLuizaHistoryPriceFaker.GetListFaker(MIN_AMOUNT_DATA).Select(price => price.Close).ToList();
+
+        // Act
+        var macd = new MACD(faker);
+
+        // Assert
+        Assert.NotNull(macd);
+        Assert.NotNull(macd.MACDLine);
+        Assert.NotNull(macd.Signal);
+        Assert.NotNull(macd.Histogram);
+    }
+
+    [Fact]
+    public void Should_Throws_Exception_When_Calculates_MCAD()
+    {
+        // Arrange
+        int ERRO_MIN_DATA = 33;
+        var faker = MagazineLuizaHistoryPriceFaker.GetListFaker(ERRO_MIN_DATA).Select(price => price.Close).ToList();
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => new MACD(faker));
+        Assert.Equal("Não há dados suficientes para gerar um MACD.", exception.Message);
+    }
+}
