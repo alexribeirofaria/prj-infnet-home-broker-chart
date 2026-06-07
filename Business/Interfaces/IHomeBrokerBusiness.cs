c804e059eb2b059d46e2f357bc091db2ee9c8681
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:54:51 -0300

    feat: adiciona contrato de negócio do HomeBroker
    ## Descrição
    
    - Define a interface da camada de negócio para consulta de dados do HomeBroker.
    - Impacto: desacopla consumidores da implementação concreta do serviço.

---
+++ Business/Interfaces/IHomeBrokerBusiness.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Charts.Agreggates;
+using Domain.Charts.ValueObject;
+
+namespace Business.Interfaces;
+public interface IHomeBrokerBusiness
+{
+    public Task<List<MagazineLuizaHistoryPrice>> GetHistoryData(Period period);
+    public Task<Sma> GetSMA(Period period);    
+    public Task<Ema> GetEMA(int periodDays, Period period);
+    public Task<MACD> GetMACD(Period period);
+    public Task<MemoryStream> GenerateExcelHistory(Period period);
+
+}
