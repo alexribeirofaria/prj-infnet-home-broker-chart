235f48f46eee40f37747d1fc66ceffee204dee6a
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:55:04 -0300

    test: adiciona testes do histórico da Magazine Luiza
    ## Descrição
    
    - Adiciona testes para o agregado de histórico de preços.
    - Impacto: valida a composição dos dados usados pela aplicação.

---
+++ HomeBrokerXUnit/Domain/MagazineLuizaHistoryPriceTest.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Charts.Agreggates;
+using HomeBrokerXUnit.__mock__;
+
+namespace Domain;
+
+public sealed class MagazineLuizaHistoryPriceTest
+{
+    [Fact]
+    public void Should_Create_Instance_MagazineLuizaHistoryPrice()
+    {
+        // Arrange
+        var faker = MagazineLuizaHistoryPriceFaker.GetNewFaker();
+        var date = faker.Date;
+        var open = faker.Open;
+        var high = faker.High;
+        var low = faker.Low;
+        var close = faker.Close;
+        var adjClose = faker.AdjClose;
+        var volume = faker.Volume;
+
+        // Act
+        var historyPrice = new MagazineLuizaHistoryPrice(date, open, high, low, close, adjClose, volume);
+
+        // Assert
+        Assert.NotNull(historyPrice);
+        Assert.Equal(date, historyPrice.Date);
+        Assert.Equal(open, historyPrice.Open);
+        Assert.Equal(high, historyPrice.High);
+        Assert.Equal(low, historyPrice.Low);
+        Assert.Equal(close, historyPrice.Close);
+        Assert.Equal(adjClose, historyPrice.AdjClose);
+        Assert.Equal(volume, historyPrice.Volume);
+    }
+}
