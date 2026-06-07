02778526ecbed597fd69672568aabcde5a868a05
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:55:05 -0300

    feat: adiciona contrato do repositório HomeBroker
    ## Descrição
    
    - Define interface para consulta de históricos no repositório.
    - Impacto: permite substituir a implementação de dados com menor acoplamento.

---
+++ Repository/Interfaces/IHomeBrokerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Charts.Agreggates;
+using Domain.Charts.ValueObject;
+
+namespace Repository.Interfaces;
+
+/// <summary>
+/// Interface que define operações para obter dados históricos do Magazine Luiza.
+/// </summary>
+public interface IHomeBrokerRepository
+{
+    /// <summary>
+    /// Obtém os dados históricos de preço do Magazine Luiza para o período especificado.
+    /// </summary>
+    /// <param name="period">O período para o qual os dados históricos são desejados.</param>
+    /// <returns>Uma lista de objetos <see cref="MagazineLuizaHistoryPrice"/>.</returns>
+    Task<List<MagazineLuizaHistoryPrice>> GetHistoryData(Period period);
+}
