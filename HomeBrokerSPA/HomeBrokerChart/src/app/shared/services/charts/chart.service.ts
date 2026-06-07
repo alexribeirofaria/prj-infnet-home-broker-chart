bc783807b35816905ed87313b14d67106ae0686c
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:55:02 -0300

    feat: adiciona serviço de dados de gráficos
    ## Descrição
    
    - Adiciona serviço Angular para consultar dados dos gráficos na API.
    - Impacto: centraliza comunicação HTTP usada pela tela de ações.

---
+++ HomeBrokerSPA/HomeBrokerChart/src/app/shared/services/charts/chart.service.ts
@@ -0,0 +1,46 @@
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Dayjs } from "dayjs";
+import { IMagazineLuizaHistoryPrice } from "../../interfaces";
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ChartService {
+  public routeUrl:string = 'ChartHomeBroker';
+
+  constructor(public http: HttpClient) {
+  }
+
+  async get(startDate:Dayjs| string, endDate: Dayjs | string): Promise<IMagazineLuizaHistoryPrice[]> {
+    const magazineLuizaHistoryPrice$ = this.http.get<any>(`${ this.routeUrl }/${startDate}/${endDate}`);
+    const result = await lastValueFrom(magazineLuizaHistoryPrice$) as IMagazineLuizaHistoryPrice[];
+    return result;
+  }
+
+  async getSMA(startDate:Dayjs| string, endDate: Dayjs | string): Promise<any> {
+    const getSMA$ = this.http.get<any>(`${ this.routeUrl }/getsma/${startDate}/${endDate}`);
+    const result = await lastValueFrom(getSMA$);
+    return result;
+  }
+
+  async getEMA(periodDays: number, startDate:Dayjs| string, endDate: Dayjs | string): Promise<any> {
+    const getEMA$ = this.http.get<any>(`${ this.routeUrl }/getema/${periodDays}/${startDate}/${endDate}`);
+    const result = await lastValueFrom(getEMA$);
+    return result;
+
+  }
+
+  async getMACD(startDate:Dayjs| string, endDate: Dayjs | string): Promise<any> {
+    const getMACD$ = this.http.get<any>(`${ this.routeUrl }/getmacd/${startDate}/${endDate}`);
+    const result = await lastValueFrom(getMACD$);
+    return result;
+  }
+
+  async downloadHistory(startDate: Dayjs | string, endDate: Dayjs | string): Promise<Blob> {
+    const downloadHistory$ = this.http.get(`${this.routeUrl}/DownloadHistory/${startDate}/${endDate}`, { responseType: 'blob' });
+    const result = await lastValueFrom(downloadHistory$);
+    return result;
+  }
+}
