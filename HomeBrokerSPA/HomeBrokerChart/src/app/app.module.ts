c7993d92df2d82c8a577ecd7fe8c1f2b9b36886a
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:54:54 -0300

    feat: adiciona módulo raiz Angular
    ## Descrição
    
    - Adiciona módulo raiz com declarações, imports e providers da SPA.
    - Impacto: estrutura a composição inicial do frontend.

---
+++ HomeBrokerSPA/HomeBrokerChart/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeModule } from './pages/home/home.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Acoesodule } from './pages/acoes/acoes.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PeriodFilterComponent } from './shared/components';
+import { CustomInterceptor } from './interceptors/http.interceptor.service';
+
+@NgModule({
+    declarations: [AppComponent, NavMenuComponent ],
+    providers: [ { provide: HTTP_INTERCEPTORS, useClass: CustomInterceptor, multi: true, } ],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, HttpClientModule, FormsModule, HomeModule, Acoesodule, AppRoutingModule, BrowserAnimationsModule, PeriodFilterComponent, NgbModule ],
+})
+export class AppModule { }
