88a96cf17069518cd247a1e7ebf2c7a8dad62830
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:55:05 -0300

    test: adiciona testes do repositório HomeBroker
    ## Descrição
    
    - Adiciona testes para a camada de repositório.
    - Impacto: valida a obtenção de dados consumidos pela camada de negócio.

---
+++ HomeBrokerXUnit/Repository/HomeBrokerRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Charts.Agreggates;
+using Domain.Charts.Agreggates.Factory;
+using Domain.Charts.ValueObject;
+using HomeBroker.Domain.Charts.Agreggates.Factory;
+using Moq;
+
+namespace Repository;
+public sealed class HomeBrokerRepositoryTest
+{
+    [Fact]
+    public async Task Should_Returns_HistoryData_GetHistoryData()
+    {
+        // Arrange
+        var mock = new MagazineLuizaHistoryPriceFactory();
+        var mockHomeBrokerRepository = new HomeBrokerRepository(mock);
+        var period = new Period(new DateTime(2023, 01, 01), new DateTime(2024, 01, 01));
+
+        // Act
+        var result = await mockHomeBrokerRepository.GetHistoryData(period);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<List<MagazineLuizaHistoryPrice>>(result);
+        Assert.False(result.Count == 365);
+        Assert.Equal(248, result.Count);        
+    }
+}
