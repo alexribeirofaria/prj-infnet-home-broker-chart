34aa2a53271860718941bb77cb5e160650e6bcc5
Alex Ribeiro de Faria <135660435+alexribeirofaria@github.com>
2026-06-06 22:54:56 -0300

    refactor: adiciona barrel de exportação compartilhada
    ## Descrição
    
    - Adiciona arquivo de exportação para simplificar imports do frontend.
    - Impacto: melhora organização e reutilização dos módulos compartilhados.

---
+++ HomeBrokerSPA/HomeBrokerChart/src/app/shared/components/charts/index.ts
@@ -0,0 +1,4 @@
+export *  from './line.chart/line.chart.module';
+export * from './candle.chart/candle.chart.module';
+export * from './line.chart/line.chart.component';
+export * from './candle.chart/candle.chart.component';
